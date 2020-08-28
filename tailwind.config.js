34fcee4799149c164e1ebaacbeb6cc8a8b40c9cc
Amber Smart <amber@asmart.dev>
2020-08-29 00:22:02 +0100

    Actually fixed issue with purged styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
-const mdx = require("@mdx-js/mdx");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: {
-    content: ["./src/**/*.{js,mdx}", "./next.config.js"],
-    options: {},
-  },
   theme: {
     extend: {
       fontFamily: {
